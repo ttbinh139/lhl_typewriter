7e61b326cf64e49dac9946c82f1c3cd147add1f0
ttbinh139 <ttbinh139@gmail.com>
2022-05-11 03:27:58 +0000

    Implement typewriter function

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,17 @@
 // typewriter.js
+const typewriter = function (sentence) {
+  let timer = 1000;
+  for (const char of sentence) {
+    timer += 100;
+    setTimeout(function () {
+        process.stdout.write(char);
+    }, timer);
+  }
+  setTimeout(function() {
+    process.stdout.write('\n');
+  }, timer);
+}
+
+const sentence = "hello there lorem ipsum dolor sit amet";
+typewriter(sentence);
+//typewriter(sentence);
